c30355363886e800801ca86a80c05aefee0b3289
Alex Luong <alex@alexluong.com>
2024-08-28 22:07:51 +0700

    chore: Rename param of jwt functions

--- internal/tenant/jwt.go
+++ internal/tenant/jwt.go
@@ -14,7 +14,7 @@ type jsonwebtoken struct{}
 
 var JWT = jsonwebtoken{}
 
-func (_ jsonwebtoken) New(jwtKey string, tenantID string) (string, error) {
+func (_ jsonwebtoken) New(jwtSecret string, tenantID string) (string, error) {
 	now := time.Now()
 	token := jwt.NewWithClaims(signingMethod, jwt.MapClaims{
 		"iss": issuer,
@@ -22,14 +22,14 @@ func (_ jsonwebtoken) New(jwtKey string, tenantID string) (string, error) {
 		"iat": now.Unix(),
 		"exp": now.Add(time.Hour).Unix(),
 	})
-	return token.SignedString([]byte(jwtKey))
+	return token.SignedString([]byte(jwtSecret))
 }
 
-func (_ jsonwebtoken) Verify(jwtKey string, tokenString string, tenantID string) (bool, error) {
+func (_ jsonwebtoken) Verify(jwtSecret string, tokenString string, tenantID string) (bool, error) {
 	token, err := jwt.Parse(
 		tokenString,
 		func(token *jwt.Token) (interface{}, error) {
-			return []byte(jwtKey), nil
+			return []byte(jwtSecret), nil
 		},
 		jwt.WithIssuer(issuer),
 		jwt.WithSubject(tenantID),
