4b16207d661aabb67247c431e691704e17b96d76
Alex Luong <alex@alexluong.com>
2024-10-05 02:27:22 +0700

    refactor: Use log repo over event & delivery models

--- internal/models/log_test.go
+++ internal/models/log_test.go
@@ -4,15 +4,69 @@ package models_test
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"testing"
 	"time"
 
+	"github.com/ClickHouse/clickhouse-go/v2"
 	"github.com/google/uuid"
 	"github.com/hookdeck/EventKit/internal/models"
+	"github.com/hookdeck/EventKit/internal/util/testutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func setupClickHouseConnection(t *testing.T) (clickhouse.Conn, func()) {
+	endpoint, cleanup, err := testutil.StartTestContainerClickHouse()
+	require.Nil(t, err)
+
+	conn, err := clickhouse.Open(&clickhouse.Options{
+		Addr: []string{endpoint},
+		Auth: clickhouse.Auth{
+			Database: "default",
+			Username: "default",
+			Password: "",
+		},
+		Debug: true,
+		Debugf: func(format string, v ...any) {
+			fmt.Printf(format+"\n", v...)
+		},
+	})
+	require.Nil(t, err)
+
+	ctx := context.Background()
+	err = conn.Exec(ctx, "CREATE DATABASE IF NOT EXISTS eventkit")
+	require.Nil(t, err)
+	err = conn.Exec(ctx, `
+		CREATE TABLE IF NOT EXISTS eventkit.events (
+			id String,
+			tenant_id String,
+			destination_id String,
+			topic String,
+			time DateTime,
+			data String
+		)
+		ENGINE = MergeTree
+		ORDER BY (id, time);
+	`)
+	require.Nil(t, err)
+	err = conn.Exec(ctx, `
+		CREATE TABLE IF NOT EXISTS eventkit.deliveries (
+			id String,
+			delivery_event_id String,
+			event_id String,
+			destination_id String,
+			status String,
+			time DateTime
+		)
+		ENGINE = ReplacingMergeTree
+		ORDER BY (id, time);
+	`)
+	require.Nil(t, err)
+
+	return conn, cleanup
+}
 
 func TestIntegrationLogRepo(t *testing.T) {
 	if testing.Short() {
