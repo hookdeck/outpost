e43aaf3b0bcfcd3d245a58005410e30d4db67bce
Alex Luong <alex@alexluong.com>
2024-11-12 21:04:39 +0700

    build: Run portal dev server in Docker compose

--- internal/portal/vite.config.ts
+++ internal/portal/vite.config.ts
@@ -4,13 +4,14 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig(({ mode }) => {
   let config: UserConfig = {
     plugins: [react()],
+    server: {
+      port: 3334,
+    },
   };
 
   if (mode === "development") {
-    config.server = {
-      proxy: {
-        "/api": "http://localhost:3333",
-      },
+    config.server!.proxy = {
+      "/api": "http://localhost:3333",
     };
   }
 
