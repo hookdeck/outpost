7b58827704ebdad6ea0c1d6141b4eaaf075a11f1
Alex Luong <alex@alexluong.com>
2024-11-17 09:56:48 +0700

    refactor: Use shared AWS util for deliverymq infra methods

--- internal/deliverymq/infra_aws.go
+++ internal/deliverymq/infra_aws.go
@@ -2,73 +2,36 @@ package deliverymq
 
 import (
 	"context"
-	"errors"
 
-	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
-	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/smithy-go"
 	"github.com/hookdeck/outpost/internal/mqs"
+	"github.com/hookdeck/outpost/internal/util/awsutil"
 )
 
 type DeliveryAWSInfra struct {
 	config *mqs.AWSSQSConfig
 }
 
-var _ DeliveryInfra = &DeliveryAWSInfra{}
-
 func (i *DeliveryAWSInfra) DeclareInfrastructure(ctx context.Context) error {
-	creds, err := i.config.ToCredentials()
+	sqsClient, err := awsutil.SQSClientFromConfig(ctx, i.config)
 	if err != nil {
 		return err
 	}
-
-	sdkConfig, err := config.LoadDefaultConfig(ctx,
-		config.WithRegion(i.config.Region),
-		config.WithCredentialsProvider(creds),
-	)
-	if err != nil {
+	if _, err := awsutil.EnsureQueue(ctx, sqsClient, i.config.Topic, createQueue); err != nil {
 		return err
 	}
-
-	sqsClient := sqs.NewFromConfig(sdkConfig, func(o *sqs.Options) {
-		if i.config.Endpoint != "" {
-			o.BaseEndpoint = aws.String(i.config.Endpoint)
-		}
-	})
-
-	_, err = ensureQueue(ctx, sqsClient, i.config.Topic)
-	return err
+	return nil
 }
 
-func NewDeliveryAWSInfra(config *mqs.AWSSQSConfig) *DeliveryAWSInfra {
+func NewDeliveryAWSInfra(config *mqs.AWSSQSConfig) DeliveryInfra {
 	return &DeliveryAWSInfra{
 		config: config,
 	}
 }
 
-func ensureQueue(ctx context.Context, sqsClient *sqs.Client, queueName string) (string, error) {
-	queue, err := sqsClient.GetQueueUrl(ctx, &sqs.GetQueueUrlInput{
+func createQueue(ctx context.Context, sqsClient *sqs.Client, queueName string) (*sqs.CreateQueueOutput, error) {
+	return sqsClient.CreateQueue(ctx, &sqs.CreateQueueInput{
 		QueueName: aws.String(queueName),
 	})
-	if err != nil {
-		var apiErr smithy.APIError
-		if errors.As(err, &apiErr) {
-			switch apiErr.(type) {
-			case *types.QueueDoesNotExist:
-				createdQueue, err := sqsClient.CreateQueue(ctx, &sqs.CreateQueueInput{
-					QueueName: aws.String(queueName),
-				})
-				if err != nil {
-					return "", err
-				}
-				return *createdQueue.QueueUrl, nil
-			default:
-				return "", err
-			}
-		}
-		return "", err
-	}
-	return *queue.QueueUrl, nil
 }
