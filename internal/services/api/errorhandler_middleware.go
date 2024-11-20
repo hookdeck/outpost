e1af46c5f464da8332bb561c0541c562fa80df22
Alex Luong <alex@alexluong.com>
2024-11-20 16:48:54 +0700

    fix: Parse error response

--- internal/services/api/errorhandler_middleware.go
+++ internal/services/api/errorhandler_middleware.go
@@ -12,6 +12,25 @@ import (
 	"go.uber.org/zap"
 )
 
+func ErrorHandlerMiddleware(logger *otelzap.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Next()
+
+		err := c.Errors.Last()
+		if err == nil {
+			return
+		}
+
+		var errorResponse ErrorResponse
+		errorResponse.Parse(err.Err)
+
+		if errorResponse.Code > 499 {
+			logger.Ctx(c.Request.Context()).Error("internal server error", zap.Error(errorResponse.Err))
+		}
+		handleErrorResponse(c, errorResponse)
+	}
+}
+
 type ErrorResponse struct {
 	Err     error       `json:"-"`
 	Code    int         `json:"-"`
@@ -23,59 +42,42 @@ func (e ErrorResponse) Error() string {
 	return e.Message
 }
 
-func NewErrInternalServer(err error) ErrorResponse {
-	return ErrorResponse{
-		Err:     err,
-		Code:    http.StatusInternalServerError,
-		Message: "internal server error",
+func (e *ErrorResponse) Parse(err error) {
+	if validationErrors, ok := err.(validator.ValidationErrors); ok {
+		out := map[string]string{}
+		for _, err := range validationErrors {
+			out[err.Field()] = err.Tag()
+		}
+		e.Code = -1
+		e.Message = "validation error"
+		e.Data = out
+		return
 	}
+	if isInvalidJSON(err) {
+		e.Code = http.StatusBadRequest
+		e.Message = "invalid JSON"
+		return
+	}
+	e.Message = err.Error()
 }
 
-func ErrorHandlerMiddleware(logger *otelzap.Logger) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Next()
-
-		err := c.Errors.Last()
-		if err == nil {
-			return
-		}
-
-		var errorResponse ErrorResponse
-		if errors.As(err.Err, &errorResponse) {
-			if errorResponse.Code > 499 {
-				logger.Ctx(c.Request.Context()).Error("internal server error", zap.Error(errorResponse.Err))
-			}
-			handleErrorResponse(c, errorResponse)
-			return
-		}
-
-		if isInvalidJSON(err) {
-			handleErrorResponse(c, ErrorResponse{
-				Code:    http.StatusBadRequest,
-				Message: "invalid JSON",
-			})
-			return
-		}
-
-		var validationErrors validator.ValidationErrors
-		if errors.As(err.Err, &validationErrors) {
-			errorResponse := ErrorResponse{}
-			errorResponse.Parse(validationErrors)
-			handleErrorResponse(c, errorResponse)
-			return
-		}
-	}
+func isInvalidJSON(err error) bool {
+	var syntaxError *json.SyntaxError
+	var unmarshalTypeError *json.UnmarshalTypeError
+	return errors.Is(err, io.EOF) ||
+		errors.Is(err, io.ErrUnexpectedEOF) ||
+		errors.As(err, &syntaxError) ||
+		errors.As(err, &unmarshalTypeError)
 }
 
 func handleErrorResponse(c *gin.Context, response ErrorResponse) {
 	c.JSON(response.Code, response)
 }
 
-func isInvalidJSON(err *gin.Error) bool {
-	var syntaxError *json.SyntaxError
-	var unmarshalTypeError *json.UnmarshalTypeError
-	return errors.Is(err.Err, io.EOF) ||
-		errors.Is(err.Err, io.ErrUnexpectedEOF) ||
-		errors.As(err.Err, &syntaxError) ||
-		errors.As(err.Err, &unmarshalTypeError)
+func NewErrInternalServer(err error) ErrorResponse {
+	return ErrorResponse{
+		Err:     err,
+		Code:    http.StatusInternalServerError,
+		Message: "internal server error",
+	}
 }
--- internal/services/api/validate.go
+++ internal/services/api/validate.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 )
 
 func AbortWithError(c *gin.Context, code int, err error) {
@@ -17,18 +16,4 @@ func AbortWithValidationError(c *gin.Context, err error) {
 	errorResponse := ErrorResponse{}
 	errorResponse.Parse(err)
 	AbortWithError(c, http.StatusUnprocessableEntity, errorResponse)
-}
-
-func (e *ErrorResponse) Parse(err error) {
-	if validationErrors, ok := err.(validator.ValidationErrors); ok {
-		out := map[string]string{}
-		for _, err := range validationErrors {
-			out[err.Field()] = err.Tag()
-		}
-		e.Code = -1
-		e.Message = "validation error"
-		e.Data = out
-		return
-	}
-	e.Message = err.Error()
 }
