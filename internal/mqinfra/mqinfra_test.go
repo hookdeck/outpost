d60d20d90e9475d7682cf09c56e9796458a0933a
Alex Luong <alex@alexluong.com>
2024-11-27 16:30:36 +0700

    feat: DLQ for RabbitMQ

--- internal/mqinfra/mqinfra_test.go
+++ internal/mqinfra/mqinfra_test.go
@@ -16,6 +16,7 @@ import (
 )
 
 func TestIntegrationMQInfra_RabbitMQ(t *testing.T) {
+	t.Parallel()
 	t.Cleanup(testinfra.Start(t))
 
 	mqConfig := mqs.QueueConfig{
@@ -33,43 +34,136 @@ func TestIntegrationMQInfra_RabbitMQ(t *testing.T) {
 		require.NoError(t, mqinfra.TeardownMQ(ctx, mqConfig))
 	})
 
-	mq := mqs.NewQueue(&mqConfig)
-	cleanup, err := mq.Init(ctx)
-	require.NoError(t, err)
-	t.Cleanup(cleanup)
-	subscription, err := mq.Subscribe(ctx)
-	require.NoError(t, err)
-	t.Cleanup(func() {
-		subscription.Shutdown(ctx)
-	})
-	msgchan := make(chan *testutil.MockMsg)
-	go func() {
-		for {
-			msg, err := subscription.Receive(ctx)
-			if err != nil {
-				log.Println(err)
-				return
+	t.Run("should create queue", func(t *testing.T) {
+		mq := mqs.NewQueue(&mqConfig)
+		cleanup, err := mq.Init(ctx)
+		require.NoError(t, err)
+		t.Cleanup(cleanup)
+		subscription, err := mq.Subscribe(ctx)
+		require.NoError(t, err)
+		t.Cleanup(func() {
+			subscription.Shutdown(ctx)
+		})
+		msgchan := make(chan *testutil.MockMsg)
+		go func() {
+			for {
+				msg, err := subscription.Receive(ctx)
+				if err != nil {
+					log.Println(err)
+					return
+				}
+				log.Println("Received message", msg)
+				msg.Ack()
+				mockMsg := &testutil.MockMsg{}
+				if err := mockMsg.FromMessage(msg); err != nil {
+					log.Println("Error parsing message", err)
+				} else {
+					msgchan <- mockMsg
+				}
 			}
-			log.Println("Received message", msg)
-			msg.Ack()
-			mockMsg := &testutil.MockMsg{}
-			if err := mockMsg.FromMessage(msg); err != nil {
-				log.Println("Error parsing message", err)
-			} else {
-				msgchan <- mockMsg
+		}()
+
+		msg := &testutil.MockMsg{ID: uuid.New().String()}
+		require.NoError(t, mq.Publish(ctx, msg))
+
+		var receivedMsg *testutil.MockMsg
+		select {
+		case receivedMsg = <-msgchan:
+		case <-time.After(1 * time.Second):
+			require.Fail(t, "timeout waiting for message")
+		}
+
+		assert.Equal(t, msg.ID, receivedMsg.ID)
+	})
+
+	// Assertion:
+	// - When the message is nacked, it should be retried 5 times before being sent to the DLQ.
+	// - Afterwards, reading the DLQ should return the message.
+	t.Run("should create dlq queue", func(t *testing.T) {
+		mq := mqs.NewQueue(&mqConfig)
+		cleanup, err := mq.Init(ctx)
+		require.NoError(t, err)
+		t.Cleanup(cleanup)
+		subscription, err := mq.Subscribe(ctx)
+		require.NoError(t, err)
+		t.Cleanup(func() {
+			subscription.Shutdown(ctx)
+		})
+		msgchan := make(chan *testutil.MockMsg)
+		go func() {
+			for {
+				msg, err := subscription.Receive(ctx)
+				if err != nil {
+					log.Println(err)
+					return
+				}
+				log.Println("Received message", msg)
+				msg.Nack()
+				mockMsg := &testutil.MockMsg{}
+				if err := mockMsg.FromMessage(msg); err != nil {
+					log.Println("Error parsing message", err)
+				} else {
+					msgchan <- mockMsg
+				}
+			}
+		}()
+
+		msg := &testutil.MockMsg{ID: uuid.New().String()}
+		require.NoError(t, mq.Publish(ctx, msg))
+
+		msgCount := 0
+	loop:
+		for {
+			select {
+			case <-msgchan:
+				msgCount++
+			case <-time.After(1 * time.Second):
+				break loop
 			}
 		}
-	}()
+		require.Equal(t, 6, msgCount)
 
-	msg := &testutil.MockMsg{ID: uuid.New().String()}
-	require.NoError(t, mq.Publish(ctx, msg))
+		dlqConfig := mqs.QueueConfig{
+			RabbitMQ: &mqs.RabbitMQConfig{
+				ServerURL: mqConfig.RabbitMQ.ServerURL,
+				Exchange:  mqConfig.RabbitMQ.Exchange + ".dlx",
+				Queue:     mqConfig.RabbitMQ.Queue + ".dlq",
+			},
+		}
 
-	var receivedMsg *testutil.MockMsg
-	select {
-	case receivedMsg = <-msgchan:
-	case <-time.After(1 * time.Second):
-		require.Fail(t, "timeout waiting for message")
-	}
-
-	assert.Equal(t, msg.ID, receivedMsg.ID)
+		dlmq := mqs.NewQueue(&dlqConfig)
+		dlsubscription, err := dlmq.Subscribe(ctx)
+		require.NoError(t, err)
+		dlmsgchan := make(chan *testutil.MockMsg)
+		go func() {
+			for {
+				msg, err := dlsubscription.Receive(ctx)
+				if err != nil {
+					log.Println(err)
+					return
+				}
+				log.Println("Received message", msg)
+				msg.Ack()
+				mockMsg := &testutil.MockMsg{}
+				if err := mockMsg.FromMessage(msg); err != nil {
+					log.Println("Error parsing message", err)
+				} else {
+					dlmsgchan <- mockMsg
+				}
+			}
+		}()
+		var dlmsg *testutil.MockMsg
+		dlmsgCount := 0
+	dlloop:
+		for {
+			select {
+			case dlmsg = <-dlmsgchan:
+				dlmsgCount++
+			case <-time.After(1 * time.Second):
+				break dlloop
+			}
+		}
+		assert.Equal(t, 1, dlmsgCount)
+		assert.Equal(t, msg.ID, dlmsg.ID)
+	})
 }
--- internal/mqinfra/rabbitmq.go
+++ internal/mqinfra/rabbitmq.go
@@ -18,38 +18,80 @@ func DeclareRabbitMQ(ctx context.Context, cfg *mqs.RabbitMQConfig) error {
 		return err
 	}
 	defer ch.Close()
+
+	dlx := cfg.Exchange + ".dlx"
+	dlq := cfg.Queue + ".dlq"
+
+	// Declare target exchange & queue
+	if err := ch.ExchangeDeclare(
+		cfg.Exchange, // name
+		"topic",      // type
+		true,         // durable
+		false,        // auto-deleted
+		false,        // internal
+		false,        // no-wait
+		nil,          // arguments
+	); err != nil {
+		return err
+	}
 	if _, err := ch.QueueDeclare(
 		cfg.Queue, // name
 		true,      // durable
 		false,     // delete when unused
 		false,     // exclusive
 		false,     // no-wait
-		nil,       // arguments
+		amqp091.Table{
+			"x-queue-type":           "quorum",
+			"x-dead-letter-exchange": dlx,
+			"x-delivery-limit":       5,
+		}, // arguments
 	); err != nil {
 		return err
 	}
-	if cfg.Exchange != "" {
-		if err := ch.ExchangeDeclare(
-			cfg.Exchange, // name
-			"topic",      // type
-			true,         // durable
-			false,        // auto-deleted
-			false,        // internal
-			false,        // no-wait
-			nil,          // arguments
-		); err != nil {
-			return err
-		}
-		if err := ch.QueueBind(
-			cfg.Queue,    // queue name
-			"",           // routing key
-			cfg.Exchange, // exchange
-			false,
-			nil,
-		); err != nil {
-			return err
-		}
+	if err := ch.QueueBind(
+		cfg.Queue,    // queue name
+		"",           // routing key
+		cfg.Exchange, // exchange
+		false,
+		nil,
+	); err != nil {
+		return err
 	}
+
+	// Declare dead-letter exchange & queue
+	if err := ch.ExchangeDeclare(
+		dlx,     // name
+		"topic", // type
+		true,    // durable
+		false,   // auto-deleted
+		false,   // internal
+		false,   // no-wait
+		nil,     // arguments
+	); err != nil {
+		return err
+	}
+	if _, err := ch.QueueDeclare(
+		dlq,   // name
+		true,  // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		amqp091.Table{
+			"x-queue-type": "quorum",
+		}, // arguments
+	); err != nil {
+		return err
+	}
+	if err := ch.QueueBind(
+		dlq, // queue name
+		"",  // routing key
+		dlx, // exchange
+		false,
+		nil,
+	); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -64,6 +106,10 @@ func TeardownRabbitMQ(ctx context.Context, cfg *mqs.RabbitMQConfig) error {
 		return err
 	}
 	defer ch.Close()
+
+	dlx := cfg.Exchange + ".dlx"
+	dlq := cfg.Queue + ".dlq"
+
 	if _, err := ch.QueueDelete(
 		cfg.Queue, // name
 		false,     // ifUnused
@@ -72,14 +118,27 @@ func TeardownRabbitMQ(ctx context.Context, cfg *mqs.RabbitMQConfig) error {
 	); err != nil {
 		return err
 	}
-	if cfg.Exchange != "" {
-		if err := ch.ExchangeDelete(
-			cfg.Exchange, // name
-			false,        // ifUnused
-			false,        // noWait
-		); err != nil {
-			return err
-		}
+	if err := ch.ExchangeDelete(
+		cfg.Exchange, // name
+		false,        // ifUnused
+		false,        // noWait
+	); err != nil {
+		return err
+	}
+	if _, err := ch.QueueDelete(
+		dlq,   // name
+		false, // ifUnused
+		false, // ifEmpty
+		false, // noWait
+	); err != nil {
+		return err
+	}
+	if err := ch.ExchangeDelete(
+		dlx,   // name
+		false, // ifUnused
+		false, // noWait
+	); err != nil {
+		return err
 	}
 	return nil
 }
