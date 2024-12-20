3f43b7c471c9af41c4e76efaddf45956797fc4b7
Alex Luong <alex@alexluong.com>
2024-12-20 16:16:04 +0700

    feat: Destination field support HTML validation properties

--- internal/destregistry/metadata/types.go
+++ internal/destregistry/metadata/types.go
@@ -29,10 +29,16 @@ type ProviderMetadata struct {
 }
 
 type FieldSchema struct {
-	Type        string `json:"type"`
-	Label       string `json:"label"`
-	Description string `json:"description"`
-	Key         string `json:"key"`
-	Required    bool   `json:"required"`
-	Sensitive   bool   `json:"sensitive,omitempty"` // Whether the field value should be obfuscated in API responses
+	Type        string  `json:"type"`
+	Label       string  `json:"label"`
+	Description string  `json:"description"`
+	Key         string  `json:"key"`
+	Required    bool    `json:"required"`
+	Sensitive   bool    `json:"sensitive,omitempty"` // Whether the field value should be obfuscated in API responses
+	Min         *int    `json:"min,omitempty"`       // Minimum value for numeric fields
+	Max         *int    `json:"max,omitempty"`       // Maximum value for numeric fields
+	Step        *int    `json:"step,omitempty"`      // Step value for numeric fields
+	MinLength   *int    `json:"minlength,omitempty"` // Minimum length for text fields
+	MaxLength   *int    `json:"maxlength,omitempty"` // Maximum length for text fields
+	Pattern     *string `json:"pattern,omitempty"`   // Regular expression pattern for text fields
 }
