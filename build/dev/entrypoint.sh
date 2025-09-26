4435f75fc98e30d9efaf0de01328801e0c034685
Alex Luong <alex@alexluong.com>
2025-09-26 19:17:24 +0700

    chore: dev log

--- build/dev/entrypoint.sh
+++ build/dev/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+echo "- Running: go run ./cmd/outpost-migrate-redis init --current"
+
 go run ./cmd/outpost-migrate-redis init --current
+
+echo "- Running: air serve"
 
 exec air serve
