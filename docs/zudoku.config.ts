d22e98f62c470e73746a6b7310bc5dc8efc54b6f
Alex Luong <alex@alexluong.com>
2025-12-17 19:02:59 +0700

    docs: destinations documentation

--- docs/zudoku.config.ts
+++ docs/zudoku.config.ts
@@ -110,7 +110,6 @@ const config: ZudokuConfig = {
         collapsible: false,
         items: [
           { type: "doc", id: "features/multi-tenant-support" },
-          { type: "doc", id: "features/destinations" },
           { type: "doc", id: "features/topics" },
           { type: "doc", id: "features/filter" },
           { type: "doc", id: "features/publish-events" },
@@ -124,6 +123,23 @@ const config: ZudokuConfig = {
             label: "SDKs",
             id: "sdks",
           },
+        ],
+      },
+      {
+        type: "category",
+        label: "Destinations",
+        link: "destinations",
+        collapsed: false,
+        collapsible: false,
+        items: [
+          { type: "doc", id: "destinations/webhook" },
+          { type: "doc", id: "destinations/hookdeck" },
+          { type: "doc", id: "destinations/aws-kinesis", label: "AWS Kinesis" },
+          { type: "doc", id: "destinations/aws-sqs", label: "AWS SQS" },
+          { type: "doc", id: "destinations/aws-s3", label: "AWS S3" },
+          { type: "doc", id: "destinations/azure-service-bus", label: "Azure Service Bus" },
+          { type: "doc", id: "destinations/gcp-pubsub", label: "GCP Pub/Sub" },
+          { type: "doc", id: "destinations/rabbitmq", label: "RabbitMQ" },
         ],
       },
       {
