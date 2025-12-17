e4658546b71a0414bdae68019b4a8ee151ab70e0
Alex Luong <alex@alexluong.com>
2025-12-17 14:49:23 +0700

    refactor: portal sidebar (#602)
    * refactor: sidebar component
    
    * refactor: sidebar api

---
+++ internal/portal/src/common/ConfigurationGuide/ConfigurationGuide.tsx
@@ -0,0 +1,12 @@
+import { DestinationTypeReference } from "../../typings/Destination";
+import Markdown from "react-markdown";
+
+export const ConfigurationGuide = ({
+  type,
+}: {
+  type: DestinationTypeReference;
+}) => {
+  return <Markdown>{type.instructions}</Markdown>;
+};
+
+export default ConfigurationGuide;
