67c7de4385b46c0f77e2ebf46c88dd99432cad4f
Alex Luong <alex.luong@hookdeck.com>
2026-09-09 23:54:15 +0700

    feat(redis): configurable connection pool size (#1055)

--- internal/config/validation.go
+++ internal/config/validation.go
@@ -108,6 +108,9 @@ func (c *Config) validateRedis() error {
 	if c.Redis.Host == "" {
 		return ErrMissingRedis
 	}
+	if c.Redis.PoolSize < 0 {
+		return ErrInvalidRedisPoolSize
+	}
 	return nil
 }
 
